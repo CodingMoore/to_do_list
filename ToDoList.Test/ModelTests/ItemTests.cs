aa846b9b0f753a035b7f52656b80b123d26b146c
Randel M <Randel.C.Moore@gmail.com>
2020-12-09 11:35:17 -0800

    add test methods for creating and adding to a list
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Nataliya Zhuravleva <natalindria@gmail.com>

--- ToDoList.Test/ModelTests/ItemTests.cs
+++ ToDoList.Test/ModelTests/ItemTests.cs
@@ -6,8 +6,42 @@ namespace ToDoList.Tests
   [TestClass]
   public class ItemTests
   {
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
 
-    // Test methods will go here.
+    [TestMethod]
+    public void GetDescription_ReturnsDescriptioin_String()
+    {
+      string description  = "Walk the dog.";
+      Item newItem = new Item(description);
+      string result = newItem.Description;
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "Walk the Dog.";
+      Item newItem = new Item(description);
+
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      List<Item> newList = new List<Item> { };
+      List<Item> result = new Item.GetAll { };
+      CollectionAssert.AreEqual(newList, result);
+    }
 
   }
 }
