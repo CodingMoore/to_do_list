d9353e4b3b8890a7451c9d272b959799ccc0a604
Randel M <Randel.C.Moore@gmail.com>
2020-12-09 16:15:15 -0800

    add additional logic for viewing list items
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Nataliya Zhuravleva <natalindria@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,47 +1,38 @@
 using System;
 using System.Collections.Generic;
 
-namespace ToDoList
+namespace ToDoList.Models
 {
 
   public class Program
   {
     public static void Main()
     {
-      List<Item> newList = new List<Item> { };
-      
-      static void Ui()
+      Console.WriteLine("Welcome to the To Do List.");
+      Console.WriteLine("Would you like to add an item to your list or view your list? ['add', 'view']");
+      string response = Console.ReadLine().ToLower();
+
+      if (response == "add" || response == "Add")
       {
-        Console.WriteLine("Welcome to the To Do List.");
-        Console.WriteLine("Would you like to add an item to your list or view your list? ['add', 'view']");
-        string response = Console.ReadLine();
-
-        if (response == "add" || response == "Add")
-        {
-          Console.WriteLine("What would you like to add?")
-          string chore = Console.ReadLine();
-          Item chore = new Item(chore);
-          chore.Add();
-
-        }
-        else if (response == "view" || response == "View")
-        {
-
-        }
-        else
-        {
-          Console.WriteLine("That is not a option!");
-        }
-
-
-
-
-        DllNotFoundException Add
+        Console.WriteLine("What would you like to add?");
+        string newChore = Console.ReadLine();
+        Item chore = new Item(newChore);
+              
       }
+      else if (response == "view" || response == "View")
+      {
+        Console.WriteLine("This is your list so far.");
+        foreach (string i in Item)
+        {
+            Console.WriteLine(Item(i));
+        }
+      }
+      else
+      {
+        Console.WriteLine("That is not a option!");
+      }
+      
+    Main();     
     }
   }
 }
-  string descritpion01 = "Walk the dog";
-  // string descritpion02 = "Wash the dishes";
-  Item newItem1 = new Item(descritpion01);
-     // Item newItem2 = new Item(descritpion02);
