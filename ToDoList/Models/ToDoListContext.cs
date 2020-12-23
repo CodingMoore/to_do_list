fd9a6585fa1e3bcc658f7ae9ce664b88f500e270
Randel M <Randel.C.Moore@gmail.com>
2020-12-23 12:02:49 -0800

    update the project to include categories, edit and delete.

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -5,8 +5,23 @@ namespace ToDoList.Models
 {
   public class ToDoListContext : DbContext
   {
+    public virtual DbSet<Category> Categories { get; set; }
     public DbSet<Item> Items { get; set; }
 
     public ToDoListContext(DbContextOptions options) : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
   }
 }
+
+
+
+// 2018-05-03 02:36:54.480
+
+// 2020-12-25 20:00:00
+
+
+// DateTime myDateTime = DateTime.Now;
+// string sqlFormattedDate = myDateTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
